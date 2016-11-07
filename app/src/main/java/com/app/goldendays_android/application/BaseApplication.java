5395d58beb2d4a29a61dd479013908fdd6d8c47b
王立强 <wangliqiangorz@163.com>
2016-11-07 11:06:23 +0800

    添加基类

--- app/src/main/java/com/app/goldendays_android/application/BaseApplication.java
+++ app/src/main/java/com/app/goldendays_android/application/BaseApplication.java
@@ -1,8 +1,45 @@
 package com.app.goldendays_android.application;
 
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by 王立强 on 2016/11/7.
  */
 
-public class BaseApplication {
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+
+        //初始化
+        OkGo.init(this);
+
+        try {
+            //全局参数设置
+            OkGo.getInstance()
+                    //打开调试开关
+                    .debug("error",true)
+                    //设置超时时间，默认60秒
+                    .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)
+                    .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)
+                    .setWriteTimeOut(OkGo.DEFAULT_MILLISECONDS)
+                    //设置全局缓存，默认无缓存
+                    .setCacheMode(CacheMode.NO_CACHE)
+                    //设置全局缓存时间，默认永不过期
+                    .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
